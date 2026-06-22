99359a0e205cdffbbab9d94fc2fb643d3a42af54
aurusxd <johndoe@email.com>
2026-06-22 16:15:08 +0700

    feat: add loader service and

--- backend/services/loader_service.py
+++ backend/services/loader_service.py
@@ -1,9 +1,22 @@
 from langchain_community.document_loaders import PyMuPDFLoader
-from log import log
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from backend.database.models.document import Document
+from backend.log import log
+from backend.services.document_service import document_service as ds
 
 
 class DocumentLoader:
-    async def document_loader(self, document_id: int):
-        loader = PyMuPDFLoader(self.ds.get_document_by_id(document_id))
-        documents = loader.load()
-        log.info(f"Страниц найдено: {len(documents)}")
+    def document_loader(self, document_id: int) -> list[Document]:
+        """
+        Принимает документ айди и возвращает чанки документа.
+
+        Возвращает: чанки документа
+
+        """
+        loader = PyMuPDFLoader(ds.get_document_by_id(document_id))
+        docs = loader.load()
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=700, chunk_overlap=50)
+        return text_splitter.split_documents(docs)
+
+loader_service = DocumentLoader()
