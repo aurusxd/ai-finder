f49b59bf581eff5aa126be472bf2e80af94b7f3d
aurusxd <johndoe@email.com>
2026-06-23 15:29:14 +0700

    feat: add ollama service

---
+++ backend/services/ollama_service.py
@@ -0,0 +1,29 @@
+from langchain_ollama import ChatOllama
+
+
+class OlammaService:
+    def __init__(self):
+        self.llm = ChatOllama(
+            model="qwen2.5",
+            temperature=0.2,
+        )
+
+    async def ask(self, question: str):
+        return self.llm.invoke(question)
+
+    def answer_by_context(self, question: str, context: str) -> str:
+
+        prompt = f"""
+            Контекст: {context}
+
+            Вопрос: {question}
+
+            Ответь только на основе контекста.
+            """
+
+        response = self._model.invoke(prompt)
+
+        return response.content
+
+
+ollama_service = OlammaService()
